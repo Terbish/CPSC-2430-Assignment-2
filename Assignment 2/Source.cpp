0fcd9d27eff1ad3dd2315c85ff606446b0423261
Jerry Shen <terb.avg@gmail.com>
2022-10-07 10:06:12 -0700

    Add project files.

---
+++ Assignment 2/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+void sumDigits();
+void root5();
+void doubleInt();	// double the number a
+void halfInt();	// half the number of a
+void egyptDivision();
+void ramanujan();
