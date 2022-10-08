bfb949b0f896fc506d5bb3473d1f3b245470e80d
Jerry Shen <terb.avg@gmail.com>
2022-10-08 13:27:59 -0700

    completed root5()

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -12,8 +12,24 @@ int sumDigits(int i) {
 	return (i % 10 + sumDigits(i / 10));
 }
 
+int root5(int i) {
+	if (i == 0 || i == 1)
+	{
+		return i;
+	}
+	int fin = 0;
+
+	while (fin * fin * fin * fin * fin <= i) {
+		fin++;
+	}
+	return fin - 1;
+}
 
 int doubleInt(int i) {
+	if (i == 0)
+	{
+		return i;
+	}
 	return (doubleInt(i += i));
 }
 
@@ -58,9 +74,21 @@ int egyptDivision(int dividend, int devisor)
 	return d;
 }
 
+//int ramanujan(int depth) {
+//	if (depth == 0)
+//	{
+//		return std::sqrt(6);
+//	}
+//	// if depth = n, sqrt(6 + n+1 sqrt(6+1+ n
+//	return (ramanujan())
+//	
+//}
+
 
 int main() {
-	//cout << sumDigits(1234);
-	std::cout << egyptDivision(1960, 56) << std::endl;
+	//std::cout << sumDigits(1234);
+	/*std::cout << egyptDivision(1960, 56) << std::endl;*/
+	/*std::cout << root5(250);*/
+	std::cout << ramanujan(0);
 	return 0;
 }
